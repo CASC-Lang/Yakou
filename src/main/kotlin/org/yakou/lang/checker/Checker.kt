287ff7fe0d9dc490d608adaa9300c8932b8807f6
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-06-19 16:34:08 +0800

    Check if top-level function has illegal modifier `mut`

--- src/main/kotlin/org/yakou/lang/checker/Checker.kt
+++ src/main/kotlin/org/yakou/lang/checker/Checker.kt
@@ -1,7 +1,13 @@
 package org.yakou.lang.checker
 
+import com.diogonunes.jcolor.Attribute
+import org.yakou.lang.ast.Item
+import org.yakou.lang.ast.Modifier
 import org.yakou.lang.ast.YkFile
+import org.yakou.lang.bind.TypeInfo
 import org.yakou.lang.compilation.CompilationUnit
+import org.yakou.lang.util.SpanHelper
+import org.yakou.lang.util.colorize
 
 class Checker(private val compilationUnit: CompilationUnit) {
     fun check() {
@@ -9,6 +15,32 @@ class Checker(private val compilationUnit: CompilationUnit) {
     }
 
     fun checkYkFile(ykFile: YkFile) {
-        // TODO: Implement
+        for (item in ykFile.items)
+            checkItem(item)
+    }
+
+    private fun checkItem(item: Item) {
+        when (item) {
+            is Item.Function -> checkFunction(item)
+            is Item.Package -> {
+                if (item.items != null)
+                    for (innerItem in item.items)
+                        checkItem(innerItem)
+            }
+        }
+    }
+
+    private fun checkFunction(function: Item.Function) {
+        // Check top-level function has illegal modifiers
+        if (function.functionInstance.ownerTypeInfo is TypeInfo.PackageClass && function.modifiers.hasModifier(Modifier.Mut)) {
+            val mutLiteral = colorize("mut", compilationUnit, Attribute.RED_TEXT())
+            val span = function.modifiers[Modifier.Mut]!!
+
+            compilationUnit.reportBuilder
+                .error(SpanHelper.expandView(span, compilationUnit.maxLineCount), "Illegal modifier `$mutLiteral`")
+                .label(span, "Top-level function cannot be mutable")
+                .color(Attribute.RED_TEXT())
+                .build().build()
+        }
     }
 }
