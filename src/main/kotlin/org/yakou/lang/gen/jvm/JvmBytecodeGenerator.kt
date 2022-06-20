e786a8db8105e468af71d9e4939c6b04906d7704
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-06-20 20:14:43 +0800

    Tweak constant field bytecode generation - Inline constant value into attribute if conditions matches

--- src/main/kotlin/org/yakou/lang/gen/jvm/JvmBytecodeGenerator.kt
+++ src/main/kotlin/org/yakou/lang/gen/jvm/JvmBytecodeGenerator.kt
@@ -1,7 +1,9 @@
 package org.yakou.lang.gen.jvm
 
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
+import org.yakou.lang.ast.Expression
 import org.yakou.lang.ast.Item
 import org.yakou.lang.ast.YkFile
 import org.yakou.lang.bind.TypeInfo
@@ -11,12 +13,19 @@ import java.io.File
 
 class JvmBytecodeGenerator(private val compilationSession: CompilationSession) {
     private val classWriters: HashMap<TypeInfo.Class, ClassWriter> = hashMapOf()
+    private val staticBlockWriters: HashMap<TypeInfo.Class, MethodVisitor> = hashMapOf()
 
     fun gen(compilationUnit: CompilationUnit) {
         genYkFile(compilationUnit.ykFile!!)
     }
 
     fun finalize() {
+        for ((_, methodVisitor) in staticBlockWriters) {
+            methodVisitor.visitInsn(Opcodes.RETURN)
+            methodVisitor.visitMaxs(-1, -1)
+            methodVisitor.visitEnd()
+        }
+
         for ((classTypeInfo, classWriter) in classWriters) {
             val bytecode = classWriter.toByteArray()
             val classFile = File(
@@ -55,15 +64,39 @@ class JvmBytecodeGenerator(private val compilationSession: CompilationSession) {
 
     private fun genConst(const: Item.Const) {
         val classWriter = getClassWriter(const.fieldInstance.ownerTypeInfo)
-        val fieldVisitor = classWriter.visitField(
-            const.fieldInstance.access,
-            const.fieldInstance.name,
-            const.fieldInstance.descriptor,
-            null,
-            null
-        )
+        // Declare static field based on value
+        if (const.expression is Expression.NumberLiteral) {
+            val fieldVisitor = classWriter.visitField(
+                const.fieldInstance.access,
+                const.fieldInstance.name,
+                const.fieldInstance.descriptor,
+                null,
+                const.expression.value
+            )
 
-        fieldVisitor.visitEnd()
+            fieldVisitor.visitEnd()
+        } else {
+            val fieldVisitor = classWriter.visitField(
+                const.fieldInstance.access,
+                const.fieldInstance.name,
+                const.fieldInstance.descriptor,
+                null,
+                null
+            )
+
+            fieldVisitor.visitEnd()
+
+            // Assign value to constant field in <clinit> method
+            val methodVisitor = getStaticBlockWriter(const.fieldInstance.ownerTypeInfo)
+
+            genExpression(methodVisitor, const.expression)
+            methodVisitor.visitFieldInsn(
+                Opcodes.PUTSTATIC,
+                const.fieldInstance.qualifiedOwnerPath,
+                const.fieldInstance.name,
+                const.fieldInstance.descriptor
+            )
+        }
     }
 
     private fun genFunction(function: Item.Function) {
@@ -76,10 +109,45 @@ class JvmBytecodeGenerator(private val compilationSession: CompilationSession) {
             null
         )
 
+        methodVisitor.visitCode()
+
         // TODO: Implement instructions
 
+        methodVisitor.visitInsn(Opcodes.RETURN) // TODO: Remove hard code
+        methodVisitor.visitMaxs(-1, -1)
         methodVisitor.visitEnd()
     }
+
+    private fun genExpression(methodVisitor: MethodVisitor, expression: Expression) {
+        when (expression) {
+            is Expression.NumberLiteral -> genNumberLiteral(methodVisitor, expression)
+            Expression.Undefined -> TODO("UNREACHABLE")
+        }
+
+        // TODO: Generate casting if Expression#originalType does not match Expression#finalType
+    }
+
+    private fun genNumberLiteral(methodVisitor: MethodVisitor, numberLiteral: Expression.NumberLiteral) {
+        methodVisitor.visitLdcInsn(numberLiteral.value)
+    }
+
+    private fun getStaticBlockWriter(clazz: TypeInfo.Class): MethodVisitor =
+        staticBlockWriters[clazz] ?: run {
+            val classWriter = getClassWriter(clazz)
+            val methodVisitor = classWriter.visitMethod(
+                Opcodes.ACC_STATIC,
+                "<clinit>",
+                "()V",
+                null,
+                null
+            )
+
+            staticBlockWriters[clazz] = methodVisitor
+
+            methodVisitor.visitCode()
+
+            methodVisitor
+        }
 
     private fun getClassWriter(clazz: TypeInfo.Class): ClassWriter =
         classWriters[clazz] ?: run {
