da9c74acbb8a2ecfa0e77055e81851b3909d0e2e
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-10-19 13:56:42 +0800

    Fix array type resolving

--- src/main/kotlin/org/yakou/lang/bind/TypeResolver.kt
+++ src/main/kotlin/org/yakou/lang/bind/TypeResolver.kt
@@ -3,8 +3,13 @@ package org.yakou.lang.bind
 import org.yakou.lang.ast.Type
 
 class TypeResolver(private val scope: Scope?, private val table: Table) {
-    fun resolveType(type: Type) =
-        resolveTypeVariable(type)
+    fun resolveType(type: Type): TypeInfo? {
+        return if (type is Type.Array) {
+            resolveType(type.type)?.let(TypeInfo::Array)
+        } else {
+            resolveTypeVariable(type)
+        }
+    }
 
     private fun resolveTypeVariable(type: Type): TypeInfo? =
         scope?.typeVariables?.get(type.standardizeType()) ?: resolveTypeFromTable(type)
