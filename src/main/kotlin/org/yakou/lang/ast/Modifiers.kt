e8aba7b2ed4709ed1b1db717bdcaf9aa6709f56a
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-11-18 23:49:19 +0800

    Make Modifiers extends LinkedHashMap

--- src/main/kotlin/org/yakou/lang/ast/Modifiers.kt
+++ src/main/kotlin/org/yakou/lang/ast/Modifiers.kt
@@ -3,30 +3,23 @@ package org.yakou.lang.ast
 import chaos.unity.nenggao.Span
 import org.objectweb.asm.Opcodes
 
-data class Modifiers(val modifierMap: LinkedHashMap<Modifier, Span> = linkedMapOf()) : AstNode {
+class Modifiers : LinkedHashMap<Modifier, Span>(), AstNode {
     override val span: Span? by lazy {
-        modifierMap.values.reduceOrNull(Span::expand)
+        values.reduceOrNull(Span::expand)
     }
 
     fun set(modifier: Modifier, span: Span): Boolean =
-        modifierMap[modifier]?.let { false } ?: run {
-            modifierMap[modifier] = span
+        if (containsKey(modifier)) {
+            false
+        } else {
+            this[modifier] = span
             true
         }
-
-    fun hasModifier(modifier: Modifier): Boolean =
-        modifierMap[modifier] != null
-
-    operator fun get(modifier: Modifier): Span? =
-        modifierMap[modifier]
-
-    fun isEmpty(): Boolean =
-        modifierMap.isEmpty()
 
     fun sum(vararg additionalFlags: Int): Int {
         var access = Opcodes.ACC_FINAL + Opcodes.ACC_PUBLIC + additionalFlags.sum()
 
-        for ((modifier, _) in modifierMap) {
+        for ((modifier, _) in this) {
             if (modifier is Modifier.AccessModifier) {
                 access -= Opcodes.ACC_PUBLIC
                 access += modifier.flag
