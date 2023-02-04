9e53b9d54e7cce3c1890a82118b86f9cb0d6aab3
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-05 02:20:40 +0800

    remove args argument from main method

--- src/main/kotlin/org/yakou/lang/process.kt
+++ src/main/kotlin/org/yakou/lang/process.kt
@@ -3,7 +3,7 @@ package org.yakou.lang
 import org.yakou.lang.cli.ArgumentProcessor
 import org.yakou.lang.compilation.CompilationSession
 
-fun main(args: Array<String>) {
+fun main() {
     val preference = ArgumentProcessor().processPreference()
     val session = CompilationSession(preference)
     session.compile()
