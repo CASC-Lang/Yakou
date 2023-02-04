3d51ed63f06cf1ad8140ef04e5bbdf0ad3264d66
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-05 02:20:11 +0800

    update kotlin to 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.8.10"
     id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
 }
 
